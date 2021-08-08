ccc08f1dcea82d7478b0de0c6f479b643594d670
jonathan <uuupah@gmail.com>
2021-08-08 15:20:02 +0000

    plan out current todos

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import os
 import discord
 
+# from PIL import image
+
 client = discord.Client()
 token = os.environ['TOKEN']
+id = os.environ['CLIENTID']
+#TODO load overlay image
+# overlay = image.open('./assets/a.png')
+
 
 @client.event
 async def on_ready():
-  print(f'logged in as {client.user}')
+    print(f'logged in as {client.user}')
+
 
 @client.event
 async def on_message(msg):
-  if msg.author == client.user:
-    return
+    print(msg.content)
+    # ignore messages by the bot
+    if msg.author == client.user:
+        return
 
-  if msg.content.startswith('$test'):
-    await msg.channel.send('hello, world')
+    if msg.content == '$ping':
+        await msg.channel.send('pong')
+
+    if msg.content == f'<@!{id}>':
+        await msg.channel.send('hello, world')
+        #TODO check last x messages (ten might be a good threshold)
+        #TODO if no images in last ten, return
+        #TODO find most recent image
+        #TODO transform image with overlay
+        #TODO post image 
+        return
+
 
 client.run(token)
