e3052f429e7ba364bd0ddec672a2355be4137450
jonathan <uuupah@gmail.com>
2021-08-10 02:00:26 +0000

    add extra braindead joke functionality

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from PIL import Image
 # from PIL import image
 
 client = discord.Client()
+sunday = os.environ['SUNDAYID']
+uuupah = os.environ['UUUPAHID']
 token = os.environ['TOKEN']
+moop250 = os.environ['250MOOP']
 id = os.environ['CLIENTID']
+
 overlay = Image.open('assets/a.png')
 overlayleft = Image.open('assets/left.png')
 overlayright = Image.open('assets/right.png')
@@ -20,18 +24,34 @@ async def on_ready():
 #TODO actual error handling
 @client.event
 async def on_message(msg):
-    print(msg.content)
+    print(msg.author)
+    print(f' > {msg.content}')
+    # print(145500797235363840 == msg.author.id)
     # ignore messages by the bot
     if msg.author == client.user:
         return
 
     if msg.content == '$ping':
         await msg.channel.send('pong')
+        return
+
+    # if msg.author.id == int(sunday):
+    #   await msg.channel.send('awooga')
+    #   return
+
+    # if msg.author.id == int(uuupah):
+      # await msg.add_reaction(f'<:moop:{moop250}>')
+      # await msg.channel.send(f'<:moop:{moop250}>')
+      # return
 
     if msg.content == f'<@!{id}>':
         async for message in msg.channel.history(limit=10):
             if message.attachments:
                 if message.attachments[0].content_type.startswith("image/"):
+                    # if msg.author.id == int(sunday):
+                    #     await message.add_reaction('\N{AUBERGINE}')
+                    #     return
+
                     targetimage = Image.open(requests.get(message.attachments[0].url, stream=True).raw)
 
                     targetar = targetimage.size[0]/targetimage.size[1]
@@ -68,7 +88,7 @@ async def on_message(msg):
                         targetimage.paste(tempoverlay, (0,targetimage.size[1]-tempoverlay.size[1]),     tempoverlay)
 
                     with io.BytesIO() as image_binary:
-                      targetimage.save(image_binary, 'PNG')
+                      targetimage.save(image_binary, 'PNG', optimize=True, quality=90)
                       image_binary.seek(0)
                       await message.channel.send(file=discord.File(fp=image_binary, filename='image.png'))
 
