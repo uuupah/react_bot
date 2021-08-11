f422a5abdd607c122de1c62dc00ca4ec74fa1d69
jonathan <uuupah@gmail.com>
2021-08-11 03:25:22 +0000

    add super basic diagnostic logging

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import requests
 import io
 from PIL import Image as image
 import ast
+from datetime import datetime
+import pytz
+
+def now():
+  tz = pytz.timezone('AUSTRALIA/Adelaide')
+  return datetime.now(tz).strftime("-%H.%M.%S %d.%m.%y")
 
 client = discord.Client()
 
@@ -27,7 +33,7 @@ except:
         moop250 = environ['moop250']
         id = environ['id']
     except:
-        print('shits fucked')
+        print(f'$$ A problem has occurred during constant variable loading {now()}')
 
 # load in images
 overlay = image.open('assets/a.png')
@@ -37,7 +43,7 @@ overlay_r = image.open('assets/right.png')
 # startup message
 @client.event
 async def on_ready():
-    print(f'logged in as {client.user}')
+    print(f'logged in as {client.user} {now()}')
 
 #TODO actual error handling
 # watching for message events
@@ -61,6 +67,7 @@ async def on_message(msg):
         async for message in msg.channel.history(limit=10):
             if message.attachments:
                 if message.attachments[0].content_type.startswith("image/"):
+                    print(f'$$ Image found at {message.attachments[0].url} {now()}')
 
                     backgr = image.open(requests.get(message.attachments[0].url, stream=True).raw)
                     backgr_w = backgr.size[0] # background width
@@ -69,6 +76,8 @@ async def on_message(msg):
                     # get aspect ratios
                     backgr_ar = backgr.size[0]/backgr.size[1]
                     overlay_ar = overlay.size[0]/overlay.size[1]
+
+                    print(f'$$ Generating new image with overlay {now()}')
 
                     # if backgr image is wider than original overlay, split the image and paste the halves separately
                     if backgr_ar > overlay_ar:
@@ -93,18 +102,20 @@ async def on_message(msg):
 
                         backgr.paste(t_overlay, (0,backgr.size[1]-t_overlay.size[1]),t_overlay)
 
+                    print(f'$$ New image generation complete {now()}')
+
                     # post image
                     with io.BytesIO() as image_binary:
                       backgr.save(image_binary, 'PNG', optimize=True, quality=90)
                       image_binary.seek(0)
                       await message.channel.send(file=discord.File(fp=image_binary, filename='image.png'))
 
+                    print(f'$$ Reaction image posted {now()}')
                     break
         return
 
 
 client.run(token)
-
 
 # naughty code jail
 
