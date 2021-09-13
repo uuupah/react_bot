455bac1bf11308b7093e8cdff527b47a49ad1320
jonathan <uuupah@gmail.com>
2021-09-13 14:15:37 +0000

    fix deleting horse plinko message

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import pytz
 from datetime import datetime
 from PIL import Image as image
 
+import youtube_dl
+import asyncio
+
 def now():
   tz = pytz.timezone('AUSTRALIA/Adelaide')
   return datetime.now(tz).strftime("-%H.%M.%S %d.%m.%y")
@@ -73,7 +76,7 @@ async def on_message(msg):
 
     #HORSE PLINKO
     if msg.content == 'horse plinko':
-        if msg.channel.type == 'private':
+        if msg.channel.type != 'private':
             await msg.delete()
         await msg.channel.send(files=[discord.File('./assets/horse1.gif'),discord.File('./assets/horse2.gif')])
         return
@@ -150,11 +153,70 @@ async def on_message(msg):
           await msg.channel.send(f'<:moop:{shitmoop}>')
         return
 
-
 client.run(token)
 
 # naughty code jail
 
+###
+
+## shamelessly copied from neuron at https://stackoverflow.com/questions/56060614/how-to-make-a-discord-bot-play-youtube-audio
+
+# youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+# ytdl_format_options = {
+#     'format': 'bestaudio/best',
+#     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+#     'restrictfilenames': True,
+#     'noplaylist': True,
+#     'nocheckcertificate': True,
+#     'ignoreerrors': False,
+#     'logtostderr': False,
+#     'quiet': True,
+#     'no_warnings': True,
+#     'default_search': 'auto',
+#     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+# }
+
+# ffmpeg_options = {
+#     'options': '-vn'
+# }
+
+# ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+# class YTDLSource(discord.PCMVolumeTransformer):
+#     def __init__(self, source, *, data, volume=0.5):
+#         super().__init__(source, volume)
+
+#         self.data = data
+
+#         self.title = data.get('title')
+#         self.url = data.get('url')
+
+#     @classmethod
+#     async def from_url(cls, url, *, loop=None, stream=False):
+#         loop = loop or asyncio.get_event_loop()
+#         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+#         if 'entries' in data:
+#             # take first item from a playlist
+#             data = data['entries'][0]
+
+#         filename = data['url'] if stream else ytdl.prepare_filename(data)
+#         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+# @commands.command(pass_context=True)
+# async def play(self, ctx, *, url):
+#         print(url)
+#         server = ctx.message.guild
+#         voice_channel = server.voice_client
+
+#         async with ctx.typing():
+#             player = await YTDLSource.from_url(url, loop=self.bot.loop)
+#             ctx.voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+#         await ctx.send('Now playing: {}'.format(player.title))
+
+###
 
 # if msg.author.id == int(uuupah):
 # await msg.add_reaction(f'<:moop:{moop250}>')
